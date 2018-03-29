16efd131872a7120c1e8e174ec9ad600b988afcc
suzy baldwin <gbaldwin24@comcast.net>
2018-03-29 15:15:02 +0000

    automatically backed up by learn

--- db/migrate/20180310181544_create_dogs.rb
+++ db/migrate/20180310181544_create_dogs.rb
@@ -1,9 +1,13 @@
 class CreateDogs < ActiveRecord::Migration
   def up
-  end
+   create_table :dogs do |t|
+     t.string :name
+     t.string :breed
+   end
+ end
 
-  def down
-  end
-
+ def down
+   drop_table :dogs
+ end
 
 end
